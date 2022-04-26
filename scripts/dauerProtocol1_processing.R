68c20bf3472067ae2c6de87facc91418453ed3a0
Tim Crombie <tim.crombie@gmail.com>
2022-04-26 13:30:04 -0500

    update analysis

--- scripts/dauerProtocol1_processing.R
+++ scripts/dauerProtocol1_processing.R
@@ -9,12 +9,9 @@ setwd(glue::glue("{dirname(rstudioapi::getActiveDocumentContext()$path)}/.."))
 # use easyXpress to process raw images - run once
 #easyXpress::tidyProject(project_dir = "/Volumes/ECA_Image/Tim/20220407_dauerProtocol1")
 
-# read in the CellProfiler output from the first pilot run
-now <- data.table::fread("~/repos/dauer_assay_protocol/CellProfiler/20220407_dauerProtocol1/output/20220412_run2/NonOverlappingWorms.csv")
-now2 <- data.table::fread("/Users/tim/repos/dauer_assay_protocol/CellProfiler/20220407_dauerProtocol1/output/20220414_run5/NonOverlappingWorms.csv")
+# read in the CellProfiler output from the process_dauer.cpproj run
+now2 <- data.table::fread("~/repos/dauer_assay_protocol/CellProfiler/20220407_dauerProtocol1/output/20220414_run6/NonOverlappingWorms.csv")
 
-
-# Run1 and run2 do not have the relate objects module that lets us know the number of NonOverlappingWorms in each wormObject.
 # lets just plot the integrated RFP intensity / area for NonOverlappingWorms
 now_proc2 <- now2 %>%
   dplyr::select(assay = Metadata_Assay, date = Metadata_Date, plate = Metadata_Plate,
@@ -51,28 +48,32 @@ hist_df <- now_proc2 %>%
 
 # plot the histograms
 all_trait_hist <- ggplot(hist_df) +
-  geom_histogram(aes(x = value)) +
+  geom_histogram(aes(x = value), bins = 100) +
   facet_wrap(~name, scales = "free", ncol = 4) +
   theme_bw()
+all_trait_hist
 
 log_cv_int_hist <- ggplot(hist_df %>% dplyr::filter(name == "log_cv_int" | name == "log_cv_int2")) +
-  geom_histogram(aes(x = value)) +
+  geom_histogram(aes(x = value), bins = 100) +
   theme_bw() +
-  geom_vline(xintercept = -2.5, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -2.9, linetype = "dashed", color = "red") +
   facet_wrap(~name, scales = "free", ncol = 4)
 log_cv_int_hist
 
 cowplot::ggsave2(all_trait_hist, filename = "plots/all_measures_histogram.png", width = 7.5, height = 7.5)
+cowplot::ggsave2(log_cv_int_hist, filename = "plots/log_cv_int_measures_histogram.png", width = 7.5, height = 7.5)
 
 #===================================================================================#
 # Try polarizing to feeding vs not feeding with log_cv_int
 #===================================================================================#
 now_proc3 <- now_proc2 %>%
-  dplyr::mutate(fed = ifelse(log_cv_int2 > -2.5, T, F)) # could impirically find this line with azide treated then bead incubated worms?
+  dplyr::mutate(fed = ifelse(log_cv_int2 > -2.9, T, F)) # could impirically find this line with azide treated then bead incubated worms?
 
 # Read the rescaled RFP PNG
-img <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220412_run2/20220407-dauerProtocol1-p002-m2X_A01_w2_NonOverlappingWorms_RFP_mask.png"))
-img2 <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220412_run2/20220407-dauerProtocol1-p002-m2X_A01_w1_overlay.png"))
+img <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220414_run6/20220407-dauerProtocol1-p002-m2X_A01_w2_NonOverlappingWorms_RFP_mask.png"))
+img2 <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220414_run6/20220407-dauerProtocol1-p002-m2X_A01_w1_overlay.png"))
+img3 <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220414_run6/20220407-dauerProtocol1-p002-m2X_G07_w2_NonOverlappingWorms_RFP_mask.png"))
+img4 <- png::readPNG(glue::glue("{getwd()}/CellProfiler/20220407_dauerProtocol1/output/20220414_run6/20220407-dauerProtocol1-p002-m2X_G07_w1_overlay.png"))
 
 h<-dim(img)[1] # image height
 w<-dim(img)[2] # image width
@@ -104,18 +105,50 @@ well_img2 <- now_proc3 %>% dplyr::filter(well == "A01") %>%
   ggplot2::theme_bw()
 well_img2
 
+well_img3 <- now_proc3 %>% dplyr::filter(well == "G07") %>%
+  ggplot2::ggplot(.) +
+  ggplot2::aes(x = AreaShape_Center_X, y = AreaShape_Center_Y, fill = log_cv_int2, shape = fed) +
+  ggplot2::annotation_custom(grid::rasterGrob(img3, width=ggplot2::unit(1,"npc"), height=ggplot2::unit(1,"npc")), 0, w, 0, -h) +
+  ggplot2::scale_fill_viridis_c() +
+  ggplot2::scale_shape_manual(values = c(21,24)) +
+  ggplot2::geom_point(alpha = 0.75, size=4) +
+  ggplot2::scale_x_continuous(expand=c(0,0),limits=c(0,w)) +
+  ggplot2::scale_y_reverse(expand=c(0,0),limits=c(h,0)) +
+  ggplot2::coord_equal() +
+  ggplot2::theme_bw()
+
+well_img4 <- now_proc3 %>% dplyr::filter(well == "G07") %>%
+  ggplot2::ggplot(.) +
+  ggplot2::aes(x = AreaShape_Center_X, y = AreaShape_Center_Y, fill = log_cv_int2, shape = fed) +
+  ggplot2::annotation_custom(grid::rasterGrob(img4, width=ggplot2::unit(1,"npc"), height=ggplot2::unit(1,"npc")), 0, w, 0, -h) +
+  ggplot2::scale_fill_viridis_c() +
+  ggplot2::scale_shape_manual(values = c(21,24)) +
+  ggplot2::geom_point(alpha = 0.75, size=4) +
+  ggplot2::scale_x_continuous(expand=c(0,0),limits=c(0,w)) +
+  ggplot2::scale_y_reverse(expand=c(0,0),limits=c(h,0)) +
+  ggplot2::coord_equal() +
+  ggplot2::theme_bw()
+
 # save well
 cowplot::ggsave2(well_img, filename = "plots/A01_RFP_fed.png", width = 7.5, height = 7.5)
 cowplot::ggsave2(well_img2, filename = "plots/A01_BF_fed.png", width = 7.5, height = 7.5)
+cowplot::ggsave2(well_img3, filename = "plots/G07_BF_fed.png", width = 7.5, height = 7.5)
+cowplot::ggsave2(well_img4, filename = "plots/G07_BF_fed.png", width = 7.5, height = 7.5)
 
 # well histogram
 a01_hist <- ggplot(now_proc2 %>% dplyr::filter(well == "A01")) +
   geom_histogram(aes(x = log_cv_int2)) +
-  geom_vline(xintercept = -2.5, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = -2.9, linetype = "dashed", color = "red") +
+  theme_bw()
+
+g07_hist <- ggplot(now_proc2 %>% dplyr::filter(well == "G07")) +
+  geom_histogram(aes(x = log_cv_int2)) +
+  geom_vline(xintercept = -2.9, linetype = "dashed", color = "red") +
   theme_bw()
 
 # save the histogram
 cowplot::ggsave2(a01_hist, filename = "plots/A01_well_hist_RFP.png", width = 7.5, height = 7.5)
+cowplot::ggsave2(g07_hist, filename = "plots/G07_well_hist_RFP.png", width = 7.5, height = 7.5)
 
 #==============================================================#
 # Part 2: Process Sorter data
@@ -140,15 +173,20 @@ a1_channels <- ggplot(score_proc %>% dplyr::filter(!is.na(condition))) +
   facet_grid(~condition + name) +
   theme_bw() +
   theme(legend.position = "none")
+a1_channels
 
-cowplot::ggsave2(a1_channels, filename = "plots/dauerProtocol1_sorter_FLchannels")
+cowplot::ggsave2(a1_channels, filename = "plots/dauerProtocol1_sorter_FLchannels.png")
+
 # look at the RFP for well A01 to see if it matches our expectations
-ggplot(score_proc %>% dplyr::filter(col == "1" & row == "A")) +
+a1_channels_a01 <- ggplot(score_proc %>% dplyr::filter(col == "1" & row == "A")) +
   aes(x = TOF, y = value, fill = condition) +
   geom_point(shape = 21) +
   facet_grid(~condition + name) +
   theme_bw() +
   theme(legend.position = "none")
+a1_channels_a01
+
+cowplot::ggsave2(a1_channels_a01, filename = "plots/dauerProtocol1_sorter_FLchannels_A01.png")
 
 # EM clustering in DI condition
 score_proc_di <- as.data.frame(raw[1]) %>%
